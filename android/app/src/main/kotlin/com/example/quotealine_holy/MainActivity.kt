61ffa12c1044c1ad7a05a627c856110c4fdbdaee
nicholasjdc <nicholasdentoncheng@gmail.com>
2023-09-11 17:32:33 -0400

    flutter init

---
+++ android/app/src/main/kotlin/com/example/quotealine_holy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quotealine_holy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/base_classes/quote.dart
@@ -0,0 +1,11 @@
+class Quote {
+	String quoteID = '';
+	String quote ='';
+	String parentFolderID = '';
+
+}
+
+Map<String, String> loadQuote(String id){
+	Map<String, String> data = {};
+	return data;
+}
